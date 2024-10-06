0682f9d834cdf565167a8f52119d43c452474ae9
cgerabit <41977306+cgerabit@users.noreply.github.com>
2024-10-05 23:42:00 -0400

    En este commit se añadieron las entidades

---
+++ EmisionDeCarbonoApi.Domain/Entidades/EmisionCarbono.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmisionDeCarbonoApi.Domain.Entidades
+{
+    public class EmisionCarbono
+    {
+        public int Id { get; set; }
+        public int EmpresaId { get; set; }
+        public Empresa? Empresa { get; set; }
+
+        [StringLength(1200)]
+        public required string Descripcion { get; set; }
+        public decimal Cantidad { get; set; }
+
+        public DateTime FechaEmision { get; set; } = DateTime.UtcNow;
+        [StringLength(100)]
+        public required string TipoEmision { get; set; }
+    }
+}
---
+++ EmisionDeCarbonoApi.Domain/Entidades/Empresa.cs
@@ -0,0 +1,10 @@
+﻿namespace EmisionDeCarbonoApi.Domain.Entidades
+{
+    public class Empresa
+    {
+        public int Id { get; set; }
+        public required string Nombre { get; set; }
+
+        public List<EmisionCarbono> EmisionesDeCarbono { get; set; } = new();
+    }
+}
